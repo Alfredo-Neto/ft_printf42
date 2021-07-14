35d037aa49dadbde73de989c922f24cc13ee854e
Alfredo Braule <alfredoneto934@gmail.com>
2021-07-14 00:32:34 -0300

    fixing memory leaks

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: ade-agui <ade-agui@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 21:44:20 by ade-agui          #+#    #+#             */
-/*   Updated: 2021/07/14 00:26:57 by ade-agui         ###   ########.fr       */
+/*   Updated: 2021/07/14 00:31:31 by ade-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,18 +45,21 @@ void print_i_d(t_flags fl, va_list args, int *len)
 {
 	fl.strNum = ft_itoa(va_arg(args, int));
 	ft_putstr_len(fl.strNum, len);
+	free(fl.strNum);
 }
 
 void print_u(t_flags fl, va_list args, int *len)
 {
 	fl.strNum = ft_uitoa(va_arg(args, int));
 	ft_putstr_len(fl.strNum, len);
+	free(fl.strNum);
 }
 
 void print_p(t_flags fl, va_list args, int *len)
 {
 	fl.strNum = ft_int_to_hex(va_arg(args, unsigned long int));
 	ft_putstr_len_p(fl.strNum, len);
+	free(fl.strNum);
 }
 
 //para imprimir integer:
